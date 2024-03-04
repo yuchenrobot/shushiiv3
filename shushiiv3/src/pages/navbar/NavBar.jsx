bf50d55ff3f71378d3f581e36ed9679c55bdc342
yuchenrobot <78885810+yuchenrobot@users.noreply.github.com>
2024-03-04 22:06:44 +0800

    NavBar.jsx

--- shushiiv3/src/pages/navbar/NavBar.jsx
+++ shushiiv3/src/pages/navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React} from 'react';
 import './navbar.css'
 import etherscan from "../../assets/social-media-icons/etherscan.png";
 import twitter from "../../assets/social-media-icons/twitter.png";
